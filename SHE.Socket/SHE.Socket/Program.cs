a6c72d70ab145aab7663ade59307239c5ef6e933
husm <steven.husm@gmail.com>
2016-10-30 19:39:30 +0800

    Add sample runnable code in HighPerformanceSocket folder.

--- SHE.Socket/SHE.Socket/Program.cs
+++ SHE.Socket/SHE.Socket/Program.cs
@@ -9,88 +9,86 @@ namespace SHE.GprsSocket
 {
     class Program
     {
-        /// <summary>
-        /// Determins the number of <see cref="SocketAsyncEventArg"/> objects put in the pool
-        /// of objects for receive/send. 
-        /// </summary>
-        public const Int32 MAX_CONNECTIONS = 3000;
+        //This variable determines the number of
+        //SocketAsyncEventArg objects put in the pool of objects for receive/send.
+        //The value of this variable also affects the Semaphore.
+        //This app uses a Semaphore to ensure that the max # of connections
+        //value does not get exceeded.
+        //Max # of connections to a socket can be limited by the Windows Operating System
+        //also.
+        public const Int32 maxNumberOfConnections = 3000;
 
-        /// <summary>
-        /// Listening port
-        /// </summary>
-        public const Int32 PORT = 6699;
+        //If this port # will not work for you, it's okay to change it.
+        public const Int32 port = 4444;
 
-        /// <summary>
-        /// The default buffer size for this test
-        /// </summary>
-        public const Int32 TEST_BUFFER_SIZE = 25;
+        //You would want a buffer size larger than 25 probably, unless you know the
+        //data will almost always be less than 25. It is just 25 in our test app.
+        public const Int32 testBufferSize = 25;
 
-        /// <summary>
-        /// The maximum number of ansynchronous accept operations that can be posted simultaneously.
-        /// Determines the size of the pool of SocketAsyncEventArgs objects that do accept operations.
-        /// </summary>
-        public const Int32 MAX_ACCEPT_OPS = 10;
+        //This is the maximum number of asynchronous accept operations that can be
+        //posted simultaneously. This determines the size of the pool of
+        //SocketAsyncEventArgs objects that do accept operations. Note that this
+        //is NOT the same as the maximum # of connections.
+        public const Int32 maxSimultaneousAcceptOps = 10;
 
-        /// <summary>
-        /// The size of the queue of incoming connections for the listen socket.
-        /// </summary>
-        public const Int32 BACKLOG_SIZE = 100;
+        //The size of the queue of incoming connections for the listen socket.
+        public const Int32 backlog = 100;
 
-        /// <summary>
-        /// Default buffer manager number.
-        /// 1 for receive, 1 for send.
-        /// </summary>
-        public const Int32 BUFFER_MANAGERS = 2;
+        //For the BufferManager
+        public const Int32 opsToPreAlloc = 2;    // 1 for receive, 1 for send
 
-        /// <summary>
-        /// Allow excess SocketAsyncEventArg objects in pool.
-        /// </summary>
-        public const Int32 ALLOW_EXCESS_SAEA = 1;
+        //allows excess SAEA objects in pool.
+        public const Int32 excessSaeaObjectsInPool = 1;
 
-        /// <summary>
-        /// The number must be the same as the value on the client.
-        /// Tells what size the message prefix will be.
-        /// 4 is the length of 32 bit integer.
-        /// </summary>
-        public const Int32 RECEIVE_PREFIX_LENGTH = 4;
-        public const Int32 SEND_PREFIX_LENGTH = 4;
+        //This number must be the same as the value on the client.
+        //Tells what size the message prefix will be. Don't change this unless
+        //you change the code, because 4 is the length of 32 bit integer, which
+        //is what we are using as prefix.
+        public const Int32 receivePrefixLength = 4;
+        public const Int32 sendPrefixLength = 4;
 
         public static Int32 mainTransMissionId = 10000;
-        public static Int32 startingTid;
+        public static Int32 startingTid; //
         public static Int32 mainSessionId = 1000000000;
 
         public static List<DataHolder> listOfDataHolders;
 
-        /// <summary>
-        /// Keep a record of maximum number of simultaneous connections
-        /// that occur while the server is running. It can not be higher
-        /// than the value of <see cref="MAX_CONNECTIONS"/>
-        /// </summary>
-        public static Int32 maxConnectedClients = 0;
-
-        public static object lockerForList = new Object();
+        // To keep a record of maximum number of simultaneous connections
+        // that occur while the server is running. This can be limited by operating
+        // system and hardware. It will not be higher than the value that you set
+        // for maxNumberOfConnections.
+        public static Int32 maxSimultaneousClientsThatWereConnected = 0;
+        public static Object lockerForList = new Object();
 
         static void Main(string[] args)
         {
             try
             {
-                // Get the endpoint for the listener
-                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, PORT);
+                // Get endpoint for the listener.
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, port);
 
                 //WriteInfoToConsole(localEndPoint);
 
-                // TODO: Use config file to setup the settings
-                SocketListenerSettings theSocketListenerSettings = new SocketListenerSettings(MAX_CONNECTIONS,
-                    ALLOW_EXCESS_SAEA, BACKLOG_SIZE, MAX_ACCEPT_OPS,
-                    RECEIVE_PREFIX_LENGTH, TEST_BUFFER_SIZE, SEND_PREFIX_LENGTH, BUFFER_MANAGERS,
+                //This object holds a lot of settings that we pass from Main method
+                //to the SocketListener. In a real app, you might want to read
+                //these settings from a database or windows registry settings that
+                //you would create.
+                SocketListenerSettings theSocketListenerSettings =
+                    new SocketListenerSettings(maxNumberOfConnections,
+                    excessSaeaObjectsInPool, backlog, maxSimultaneousAcceptOps,
+                    receivePrefixLength, testBufferSize, sendPrefixLength, opsToPreAlloc,
                     localEndPoint);
 
+                Console.WriteLine("SocketListener init begin");
+
+                //instantiate the SocketListener.
                 SocketListener socketListener = new SocketListener(theSocketListenerSettings);
 
+                Console.WriteLine("SocketListener init end");
             }
             catch (Exception ex)
             {
-
+                Console.WriteLine("Error: " + ex.Message);
             }
         }
     }
